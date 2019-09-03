4b8a3fd2b7620eccf21407c301cb5a46a4c3ce8b
Suren <surendra.lama@assetowl.com>
2019-09-03 15:15:19 +0545

    ios: removed header and implementation files

--- ios/Classes/TokenServicePlugin.m
+++
@@ -1,8 +0,0 @@
-#import "TokenServicePlugin.h"
-#import <token_service/token_service-Swift.h>
-
-@implementation TokenServicePlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftTokenServicePlugin registerWithRegistrar:registrar];
-}
-@end
